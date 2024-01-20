1cc900977bba64e06c01a7fcd55c37c32cc74964
Nick Peck <nicholas.w.peck@gmail.com>
2024-01-20 13:32:16 -0800

    ability to show a message with the loading spinner

--- src/left/sharedcomponents.py
+++ src/left/sharedcomponents.py
@@ -4,12 +4,13 @@ import flet as ft
 
 
 @cache
-def loading_spinner(size=50):
+def loading_spinner(size=50, message=None):
+    controls = [ft.ProgressRing(width=size, height=size, stroke_width=2)]
+    if message is not None:
+        controls.append(ft.Text(message))
     return ft.Row([
             ft.Column(
-                [
-                    ft.ProgressRing(width=size, height=size, stroke_width=2)
-                ],
+                controls=controls,
                 alignment=ft.MainAxisAlignment.CENTER,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             )
