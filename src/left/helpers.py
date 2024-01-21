36542f0928acff2c8daacc39eb9914274e57012a
Nick Peck <nicholas.w.peck@gmail.com>
2024-01-20 21:20:30 -0800

    Adds a go_back() helper

--- src/left/helpers.py
+++ src/left/helpers.py
@@ -1,5 +1,6 @@
 from .app import LeftApp
 
+import flet as ft
 
 def make_props(*functions):
     """A little helper that converts a list of named functions into a dictionary of {name:function...}
@@ -12,6 +13,12 @@ def redirect(route: str):
     if page.route == route:
         return
     page.go(route)
+
+
+def go_back(e):
+    """e is an event originating from a control on the active view"""
+    page = LeftApp.get_app().page
+    page.on_view_pop.get_handler()(e)
 
 
 def get_page():
